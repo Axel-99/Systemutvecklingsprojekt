f24a724d4caa99a4ab7bad7bfcd8f73d8f731214
Axel-99 <56883504+Axel-99@users.noreply.github.com>
2021-03-08 14:11:59 +0100

    Add project files.

---
+++ Systemutvecklingsprojekt/Systemutvecklingsprojekt/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Systemutvecklingsprojekt.ViewModels;
+
+namespace Systemutvecklingsprojekt.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Systemutvecklingsprojekt/Systemutvecklingsprojekt/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Systemutvecklingsprojekt.Models;
+using Systemutvecklingsprojekt.ViewModels;
+
+namespace Systemutvecklingsprojekt.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
